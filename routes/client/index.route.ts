839efeba675043c7906262399d4ff0195f628066
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2024-09-26 14:35:19 +0700

    Update Private

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -2,8 +2,10 @@ import { Express } from "express";
 import { taskRoute } from "./task.route";
 import { userRoute } from "./user.route";
 
+import { requireAuth } from "../../middlewares/client/auth.middleware";
+
 export const routeApi = (app: Express) => {
     app.use("/tasks", taskRoute);
 
-    app.use("/users", userRoute);
+    app.use("/users", requireAuth, userRoute);
 }
