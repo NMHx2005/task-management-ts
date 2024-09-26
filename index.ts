cea1110ea67ad86b38c9dbb061c0be6959ab9b20
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2024-09-26 13:55:49 +0700

    Cài đặt CORS

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,6 +11,19 @@ connectDatabase();
 
 const app: Express = express();
 const port: number | string= process.env.PORT || 3000;
+
+// CORS
+// Cách 1: Tất cả các tên miền được phép truy cập
+app.use(cors())
+
+// Cách 2: Truy cập cho từng tên miền
+// var corsOptions = {
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+// End CORS
+
 
 // parse application/json
 app.use(bodyParser.json());
